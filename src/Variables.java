faf3405b6d4e0493eb032e18565344a480350438
Alex Acevedo <alex.acpe@outlook.com>
2022-05-07 19:51:31 -0500

    Variables

---
+++ src/Variables.java
@@ -0,0 +1,16 @@
+import java.sql.SQLOutput;
+
+public class Variables {
+    public static void main(String[] args) {
+        //Declarar la variable
+        int speed;
+        //asignar valor a la variable
+        speed=10;
+        System.out.println(speed);
+
+        int salari=1000;
+        String nombre="Alex";
+
+        System.out.println(nombre);
+    }
+}
